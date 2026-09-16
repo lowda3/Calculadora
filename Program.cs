75436d9c776f922ba0fe670f94e55381a88bf3b9
lowda3 <n00041141@upn.pe>
2026-09-16 12:42:49 -0500

    Creacion Modulo leer datos - precio y cantidad

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+const double porc_descuento = 0.1;
+static void LeerDatos(ref double preciox, ref int cantidadx)
+{
+  System.Console.WriteLine("Ingrese precio:");
+  preciox = double.Parse(Console.ReadLine());
+  System.Console.WriteLine("Ingrese cantidad");
+  cantidadx = int.Parse(Console.ReadLine());
+}
+
+static double CalcularSubtotal(double precio, int cantidad)
+{
+  double sub_total = precio * cantidad;
+  return sub_total;
+}
+
+static double CalcularDescuento(double Subtotal)
+{
+  return Subtotal * porc_descuento;
+}
+
+static double CalcularTotal(double Subtotal, double descuento)
+{
+  return Subtotal - descuento;
+}
+System.Console.WriteLine("Bienvenido a la calculadora de compras");
+double precio = 0;
+int cantidad = 0;
+LeerDatos(ref precio, ref cantidad);
+
+double sub_total = CalcularSubtotal(precio,cantidad);
+System.Console.WriteLine($"Subtotal:{sub_total}");
+
+CalcularDescuento(sub_total);
